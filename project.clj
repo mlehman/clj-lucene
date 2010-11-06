bca25404f364e71ff1ffe3a9bfc37d4fd21e50cf
Matt Lehman <matt.lehman@gmail.com>
2010-11-06 23:36:22 +0100

    add lucene

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-lucene "0.1.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.apache.lucene/lucene-core "3.0.2"]])
