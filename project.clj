520cbce38709c6d40dbfe1e5af97858059dc22c0
Matt Lehman <matt.lehman@gmail.com>
2010-11-06 21:07:22 +0100

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-lucene "1.0.0-SNAPSHOT"
+(defproject clj-lucene "0.1.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
