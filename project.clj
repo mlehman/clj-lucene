52da4e2007baa922d368eb9e0f722ac08dccc4d9
Matt Lehman <matt.lehman@gmail.com>
2010-11-07 11:13:56 +0100

    update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-lucene "0.1.0"
-  :description "FIXME: write"
+  :description "A Clojure library wrapping Lucene."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.lucene/lucene-core "3.0.2"]])
